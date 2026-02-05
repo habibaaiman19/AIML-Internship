3707de84080c0de71abebe56edc7a38f3d3ad8b5
habibaaiman19 <habibaaiman77@gmail.com>
2026-02-05 14:55:03 +0530

    Add files via upload

---
+++ p6.py
@@ -0,0 +1,8 @@
+fruits = ["apples","bananas","carrots","dates"]
+print("current fruits",fruits)
+fruits.append("eggs")
+print(fruits)
+fruits.remove("bananas")
+print(fruits)
+fruits.sort()
+print("updated fruits",fruits)
---
+++ pg2.py
@@ -0,0 +1,4 @@
+text = "computer science"
+print(text[::-1])
+print(text[0::3])
+print(text[8:])
