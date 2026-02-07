84266bf7d7397454914d3bbf1bbc138b291e6bdd
habibaaiman19 <habibaaiman77@gmail.com>
2026-02-07 19:47:36 +0530

    commit

---
+++ main2.py
@@ -0,0 +1,6 @@
+import math_operations
+result_power = math_operations.power(2,10)
+numbers = [10,20,20,40]
+result_avg = math_operations.average(numbers)
+print("2^10=",result_power)
+print("average=",result_avg)
