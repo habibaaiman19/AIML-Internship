2dae2fbdad95b2a02d997c2bf7c1b30d1ea2ea77
habibaaiman19 <habibaaiman77@gmail.com>
2026-02-09 15:31:54 +0530

    day-6

--- mainmarks.py
+++ mainmarks.py
@@ -3,3 +3,8 @@ marks = input("Enter marks:")
 with open("marks1.txt","a")as file:
     file.write(f"{name}-{marks}\n")
     print("marks appended successfully!")
+    
+filename = input("enter your file: ")
+with open(filename,"r")as file:
+    lines=file.readlines()
+    print("number of lines:",len(lines))
