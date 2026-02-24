80397788cc8217c7d583efec378914801d36b3ff
habibaaiman19 <habibaaiman77@gmail.com>
2026-02-24 12:45:11 +0530

    Delete p6.py

--- p6.py
+++
@@ -1,8 +0,0 @@
-fruits = ["apples","bananas","carrots","dates"]
-print("current fruits",fruits)
-fruits.append("eggs")
-print(fruits)
-fruits.remove("bananas")
-print(fruits)
-fruits.sort()
-print("updated fruits",fruits)
